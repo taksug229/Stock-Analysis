3aff0e6871fa46c602fd075a01c7aef19fc11e40
Takeshi Sugiyama <taksug229@gmail.com>
2024-06-02 22:20:25 +0900

    Added back setup script

--- cmd/setup/main.go
+++ cmd/setup/main.go
@@ -3,16 +3,18 @@ package main
 
 import (
 	"log"
+	"main/backend/api"
+	"main/backend/gcp"
 )
 
 func main() {
 	log.Println("Starting environment setup...")
 
-	// uniqueTickers := api.SaveFinancialData()
-	// api.SaveQuoteFromYahoo(uniqueTickers)
-	// gcp.UploadToGCSToBigQuery()
-	// gcp.CreateMLTable()
-	// gcp.CreateTrainTestTable()
-	// gcp.CreateModel()
+	uniqueTickers := api.SaveFinancialData()
+	api.SaveQuoteFromYahoo(uniqueTickers)
+	gcp.UploadToGCSToBigQuery()
+	gcp.CreateMLTable()
+	gcp.CreateTrainTestTable()
+	gcp.CreateModel()
 	log.Println("Environment setup complete!")
 }
