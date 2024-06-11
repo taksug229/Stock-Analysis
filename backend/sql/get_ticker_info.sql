cdcd4980d573c85ea104302d2e1ddec27cb72886
Takeshi Sugiyama <taksug229@gmail.com>
2024-06-11 22:15:42 +0900

    Fixed prediction model

--- backend/sql/get_ticker_info.sql
+++ backend/sql/get_ticker_info.sql
@@ -18,8 +18,8 @@ FROM ML.PREDICT
             a.stockprice_last_yr,
             ${LIVE_STOCK_PRICE} AS stockprice_current,
             a.stock_cagr,
-            a.52w_high,
-            a.52w_low,
+            a.high_last_yr,
+            a.low_last_yr,
             a.volume_last_yr
     FROM
     (
